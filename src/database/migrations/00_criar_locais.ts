7d01c58e5e2a71a3128d4b65d902728c05a61f0a
Ian Torquato <iantorquato2@gmail.com>
2020-12-09 14:03:54 -0300

    Adicionando timestamp

--- src/database/migrations/00_criar_locais.ts
+++ src/database/migrations/00_criar_locais.ts
@@ -2,9 +2,11 @@ import Knex from 'knex'
 
 export async function up(Knex: Knex) {
 	return Knex.schema.createTable('locais', table => {
-		table.increments('id').primary()
+		table.increments()
 		table.string('nome').notNullable()
 		table.string('imagem').notNullable()
+		table.timestamp('created_at', { precision: 0 }).defaultTo(Knex.fn.now(0))
+		table.timestamp('updated_at', { precision: 0 }).defaultTo(Knex.fn.now(0))
 	})
 }
 
--- src/database/migrations/01_criar_usuarios.ts
+++ src/database/migrations/01_criar_usuarios.ts
@@ -2,10 +2,12 @@ import Knex from 'knex'
 
 export async function up(Knex: Knex) {
 	return Knex.schema.createTable('usuarios', table => {
-		table.increments('id').primary()
+		table.increments()
 		table.integer('foto').notNullable().defaultTo(0)
 		table.string('nome').notNullable()
 		table.string('email').notNullable().unique()
+		table.timestamp('created_at', { precision: 0 }).defaultTo(Knex.fn.now(0))
+		table.timestamp('updated_at', { precision: 0 }).defaultTo(Knex.fn.now(0))
 	})
 }
 
