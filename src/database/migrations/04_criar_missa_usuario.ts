e77d75007eddb8415e23d87500bab077f53e70e4
Ian Torquato <iantorquato2@gmail.com>
2020-12-16 16:11:01 -0300

    Bug resolvido. Funcionando 100% uhuul

--- src/database/migrations/04_criar_missa_usuario.ts
+++ src/database/migrations/04_criar_missa_usuario.ts
@@ -2,19 +2,13 @@ import Knex from 'knex'
 
 export async function up(Knex: Knex) {
 	await Knex.raw(`
-		CREATE OR REPLACE FUNCTION total_pessoas_cadastradas()
-		RETURNS trigger AS $$
+		CREATE OR REPLACE FUNCTION total_pessoas_cadastradas() RETURNS trigger AS $$
 		
 		DECLARE
 			soma_quantidade_pessoas integer;
 				
 		BEGIN
-			IF ((SELECT id FROM missa_usuario FIRST)) THEN
-				soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario INNER JOIN missas
-				ON NEW.missa_id = missas.id;
-			ELSE
-				soma_quantidade_pessoas = NEW.quantidade_pessoas;
-			END IF;
+			soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario WHERE missa_id = NEW.missa_id;
 		
 			UPDATE missas SET pessoas_cadastradas = soma_quantidade_pessoas WHERE id = NEW.missa_id;
 			
@@ -30,26 +24,24 @@ export async function up(Knex: Knex) {
 		table.integer('usuario_id').notNullable().references('id').inTable('usuarios').onDelete('CASCADE')
 		table.integer('quantidade_pessoas').notNullable()
 	}).then(() => Knex.raw(`
-		CREATE TRIGGER missas_pessoas_cadastradas
-		BEFORE INSERT ON missa_usuario
-		FOR EACH ROW
+		CREATE TRIGGER missas_pessoas_cadastradas AFTER INSERT ON missa_usuario FOR EACH ROW
 		EXECUTE PROCEDURE total_pessoas_cadastradas();
 	`))
 }
 
-// BEGIN
-//         IF (TG_OP = 'DELETE') THEN
-//             INSERT INTO emp_audit SELECT 'D', now(), user, OLD.*;
-//             RETURN OLD;
-//         ELSIF (TG_OP = 'UPDATE') THEN
-//             INSERT INTO emp_audit SELECT 'U', now(), user, NEW.*;
-//             RETURN NEW;
-//         ELSIF (TG_OP = 'INSERT') THEN
-//             INSERT INTO emp_audit SELECT 'I', now(), user, NEW.*;
-//             RETURN NEW;
-//         END IF;
-//         RETURN NULL; -- result is ignored since this is an AFTER trigger
-// 		END;
+//	BEGIN
+//	 	IF (TG_OP = 'DELETE') THEN
+//	  	INSERT INTO emp_audit SELECT 'D', now(), user, OLD.*;
+//	  	RETURN OLD;
+//	 	ELSIF (TG_OP = 'UPDATE') THEN
+//	  	INSERT INTO emp_audit SELECT 'U', now(), user, NEW.*;
+//	  	RETURN NEW;
+//	 	ELSIF (TG_OP = 'INSERT') THEN
+//	  	INSERT INTO emp_audit SELECT 'I', now(), user, NEW.*;
+//	  	RETURN NEW;
+//	 	END IF;
+//	 	RETURN NULL; -- result is ignored since this is an AFTER trigger
+//	END;
 
 export async function down(Knex: Knex) {
 	return Knex.schema.dropTable('missa_usuario')
