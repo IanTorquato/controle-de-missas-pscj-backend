a8edcec951861ac1ecfdcc4feaea6d326081fc8e
Ian Torquato <iantorquato2@gmail.com>
2020-12-16 16:04:28 -0300

    Trigger para calcular o total de pessoas em uma missa -Ainda não terminada

--- src/database/migrations/04_criar_missa_usuario.ts
+++ src/database/migrations/04_criar_missa_usuario.ts
@@ -6,13 +6,18 @@ export async function up(Knex: Knex) {
 		RETURNS trigger AS $$
 		
 		DECLARE
-			missas missas;
 			soma_quantidade_pessoas integer;
 				
 		BEGIN
-			soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario WHERE NEW.missa_id = missas.id;
+			IF ((SELECT id FROM missa_usuario FIRST)) THEN
+				soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario INNER JOIN missas
+				ON NEW.missa_id = missas.id;
+			ELSE
+				soma_quantidade_pessoas = NEW.quantidade_pessoas;
+			END IF;
 		
-			UPDATE missas SET pessoas_cadastradas = soma_quantidade_pessoas;
+			UPDATE missas SET pessoas_cadastradas = soma_quantidade_pessoas WHERE id = NEW.missa_id;
+			
 			RETURN NEW;
 		END;
 		
@@ -26,10 +31,25 @@ export async function up(Knex: Knex) {
 		table.integer('quantidade_pessoas').notNullable()
 	}).then(() => Knex.raw(`
 		CREATE TRIGGER missas_pessoas_cadastradas
-		AFTER INSERT ON missa_usuario
+		BEFORE INSERT ON missa_usuario
+		FOR EACH ROW
 		EXECUTE PROCEDURE total_pessoas_cadastradas();
 	`))
 }
+
+// BEGIN
+//         IF (TG_OP = 'DELETE') THEN
+//             INSERT INTO emp_audit SELECT 'D', now(), user, OLD.*;
+//             RETURN OLD;
+//         ELSIF (TG_OP = 'UPDATE') THEN
+//             INSERT INTO emp_audit SELECT 'U', now(), user, NEW.*;
+//             RETURN NEW;
+//         ELSIF (TG_OP = 'INSERT') THEN
+//             INSERT INTO emp_audit SELECT 'I', now(), user, NEW.*;
+//             RETURN NEW;
+//         END IF;
+//         RETURN NULL; -- result is ignored since this is an AFTER trigger
+// 		END;
 
 export async function down(Knex: Knex) {
 	return Knex.schema.dropTable('missa_usuario')
