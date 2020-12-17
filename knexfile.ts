2d5388b4679888c6ed3d651e7e0716dda0a861bd
Ian Torquato <iantorquato2@gmail.com>
2020-12-17 20:18:08 -0300

    Removendo timestamps e respectiva trigger

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 import 'dotenv/config'
-import { CreateTableBuilder } from 'knex'
 
 import databaseConfig from './src/config/database'
 
@@ -11,12 +10,5 @@ module.exports = {
 	},
 	seeds: {
 		directory: path.resolve(__dirname, 'src', 'database', 'seeds')
-	},
-	onUpdateTrigger: (table: CreateTableBuilder) => {
-		return `
-			CREATE TRIGGER ${table}_updated_at
-			AFTER UPDATE ON ${table}
-			EXECUTE PROCEDURE update_timestamp();
-		`
 	}
 }
--- src/database/migrations/01_criar_usuarios.ts
+++ src/database/migrations/01_criar_usuarios.ts
@@ -1,16 +1,12 @@
 import Knex from 'knex'
 
-const { onUpdateTrigger } = require('../../../knexfile')
-
 export async function up(Knex: Knex) {
 	return Knex.schema.createTable('usuarios', table => {
 		table.increments()
 		table.integer('foto').notNullable().defaultTo(0)
 		table.string('nome').notNullable()
 		table.string('email').notNullable().unique()
-		table.timestamp('created_at', { precision: 0 }).defaultTo(Knex.fn.now(0))
-		table.timestamp('updated_at', { precision: 0 }).defaultTo(Knex.fn.now(0))
-	}).then(() => Knex.raw(onUpdateTrigger('usuarios')))
+	})
 }
 
 export async function down(Knex: Knex) {
