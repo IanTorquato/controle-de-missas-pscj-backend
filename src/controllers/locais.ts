b2d26570a1145e29aeb4989f84f3ceba285ce23c
Ian Torquato <iantorquato2@gmail.com>
2020-12-17 20:18:58 -0300

    Detalhe na listagem de locais

--- src/controllers/locais.ts
+++ src/controllers/locais.ts
@@ -7,10 +7,10 @@ class Locais {
 		try {
 			const locais = await knex('locais').select('*')
 
-			if (!locais[0]) { return response.status(404).json({ erro: 'Ainda não há nenhum dado para ser listado.' }) }
+			if (!locais[0]) { return response.status(404).json({ erro: 'Ainda não há nenhum local para ser listado.' }) }
 
 			const locaisSerializados = locais.map(local => ({
-				id: local.id, nome: local.nome, imagem_url: `${process.env.URL_BANCO}/uploads/fotosLocais/${local.imagem}`
+				...local, imagem: `${process.env.URL_BANCO}/uploads/fotosLocais/${local.imagem}`
 			}))
 
 			return response.json(locaisSerializados)
