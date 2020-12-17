2f7d744d4b980e100a98491226b00ec8777381d4
Ian Torquato <iantorquato2@gmail.com>
2020-12-17 18:14:32 -0300

    Trigger totalmente finalizada lidando com INSERT, UPDATE e DELETE

--- src/database/migrations/04_criar_missa_usuario.ts
+++ src/database/migrations/04_criar_missa_usuario.ts
@@ -8,11 +8,20 @@ export async function up(Knex: Knex) {
 			soma_quantidade_pessoas integer;
 				
 		BEGIN
-			soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario WHERE missa_id = NEW.missa_id;
-		
-			UPDATE missas SET pessoas_cadastradas = soma_quantidade_pessoas WHERE id = NEW.missa_id;
-			
-			RETURN NEW;
+			IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+				soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario WHERE missa_id = NEW.missa_id;			
+				
+				UPDATE missas SET pessoas_cadastradas = soma_quantidade_pessoas WHERE id = NEW.missa_id;
+				RETURN NEW;
+			ELSIF (TG_OP = 'DELETE') THEN
+				soma_quantidade_pessoas = SUM(quantidade_pessoas) FROM missa_usuario WHERE missa_id = OLD.missa_id;		
+				
+				IF (soma_quantidade_pessoas IS NULL) THEN soma_quantidade_pessoas = 0; END IF;
+				
+				UPDATE missas SET pessoas_cadastradas = soma_quantidade_pessoas WHERE id = OLD.missa_id;
+				
+				RETURN OLD;
+		 	END IF;
 		END;
 		
 		$$ language 'plpgsql';
@@ -24,7 +33,7 @@ export async function up(Knex: Knex) {
 		table.integer('usuario_id').notNullable().references('id').inTable('usuarios').onDelete('CASCADE')
 		table.integer('quantidade_pessoas').notNullable()
 	}).then(() => Knex.raw(`
-		CREATE TRIGGER missas_pessoas_cadastradas AFTER INSERT ON missa_usuario FOR EACH ROW
+		CREATE TRIGGER missas_pessoas_cadastradas AFTER INSERT OR UPDATE OR DELETE ON missa_usuario FOR EACH ROW
 		EXECUTE PROCEDURE total_pessoas_cadastradas();
 	`))
 }
