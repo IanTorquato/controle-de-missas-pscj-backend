07abc77372533a0c4c0b4eb9c6ccbc824b05e6bb
Ian Torquato <iantorquato2@gmail.com>
2021-01-16 18:14:31 -0300

    Função que adiciona url das fotos dos locais ao listar

---
+++ src/utils/urlImagemLocal.ts
@@ -0,0 +1,9 @@
+interface Missa {
+	local_url: string
+}
+
+const urlImagemLocal = (missas: Missa[]) => missas.map(missa => (
+	{ ...missa, local_url: `${process.env.URL_BANCO}/uploads/fotosLocais/${missa.local_url}` }
+))
+
+export { urlImagemLocal }
